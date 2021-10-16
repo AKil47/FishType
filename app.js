55751a5e06cda4fd56ae9ae0be096af36e35a111
Akilan Manivannan <amanivannan@outlook.com>
2021-10-16 16:21:47 -0500

    Added function to generate UUID

--- app.js
+++ app.js
@@ -67,9 +67,21 @@ let incrementTrial = function() {
 }
 
 
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
 // We done... let's upload
 let finishSurvey = function() {
     console.log(saved_data)
+
+    var blob = new Blob([JSON.stringify(saved_data)], {type: "application/json"})
 }
 
 
